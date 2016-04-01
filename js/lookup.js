d62c99f1302b60140b6c994349c51c25df949ad0
Garrick Sherman <backwardstg@gmail.com>
2016-04-01 13:37:23 -0500

    link searches

--- js/lookup.js
+++ js/lookup.js
@@ -1,7 +1,10 @@
 function search() {
   var word = document.getElementById('lookup-term').value;
+  
+  // Clean current messages
   var ul = document.getElementById('results');
   ul.innerHTML = '';
+  statusMsg('');
 
   if (word in data) {
     for (var i = 0; i < data[word].length; i++) {
@@ -9,13 +12,29 @@ function search() {
       appendToUl(ul, w);
     }
   } else {
-    appendToUl(ul, 'No words within one edit of '+word+'.');
-    appendToUl(ul, 'Word does not exist or is not similar to other words.');
+    statusMsg('No words within one edit of '+word+'. Word does not exist or is not similar to other words.');
   }
+}
+
+function linkSearch(word) {
+  var lookupBox = document.getElementById('lookup-term');
+  lookupBox.value = word;
+  search();
 }
 
 function appendToUl(ul, toAppend) {
   var li = document.createElement('li');
-  li.appendChild(document.createTextNode(toAppend));
+
+  a = document.createElement('a');
+  a.href = '#';
+  a.setAttribute('onClick', 'linkSearch("'+toAppend+'")');
+  li.appendChild(a);
+    
+  a.appendChild(document.createTextNode(toAppend));
   ul.appendChild(li);
+}
+
+function statusMsg(msg) {
+  var stat = document.getElementById('status');
+  stat.innerHTML = msg;
 }
