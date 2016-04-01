9c0e2af2d4b5833cdb199ce7ca3097724cff7c56
Garrick Sherman <backwardstg@gmail.com>
2016-04-01 16:05:21 -0500

    added information

--- js/lookup.js
+++ js/lookup.js
@@ -1,42 +1,54 @@
 function search() {
-  var word = document.getElementById('lookup-term').value;
+  var origWord = document.getElementById('lookup-term').value;
+  var word = origWord.toLowerCase()
+  var ul = document.getElementById('results');
   
   // Clean current messages
-  var ul = document.getElementById('results');
   ul.innerHTML = '';
   statusMsg('');
 
   if (word in data) {
-    statusMsg(data[word].length+' words found within one edit of '+word+'.');
+    statusMsg(data[word].length+' words one edit from '+bold(origWord)+'.');
 
-    for (var i = 0; i < data[word].length; i++) {
-      var w = data[word][i];
+    data[word].forEach(function(w) {
       appendToUl(ul, w);
-    }
+    });
   } else {
-    statusMsg('No words within one edit of '+word+'. Word does not exist or is not similar to other words.');
+    statusMsg('No words within one edit of '+bold(origWord)+'.');
+    var explanation = words.indexOf(word) > -1 ? 'Word is not similar to other words.' : 'Word is not known.'
+    explanation += ' <a href="about.html#why-no-words">Why?</a>';
+    statusMsg(explanation, true);
   }
 }
 
 function linkSearch(word) {
-  var lookupBox = document.getElementById('lookup-term');
-  lookupBox.value = word;
+  document.getElementById('lookup-term').value = word;
   search();
 }
 
 function appendToUl(ul, toAppend) {
+  // Create list item
   var li = document.createElement('li');
 
+  // Create link
   a = document.createElement('a');
   a.href = '#';
   a.setAttribute('onClick', 'linkSearch("'+toAppend+'")');
+
+  // Create text
+  var text = document.createTextNode(toAppend);
+
+  a.appendChild(text);
   li.appendChild(a);
-    
-  a.appendChild(document.createTextNode(toAppend));
   ul.appendChild(li);
 }
 
-function statusMsg(msg) {
+function statusMsg(msg, append = false) {
   var stat = document.getElementById('status');
+  msg = append ? stat.innerHTML + '<br>' + msg : msg;
   stat.innerHTML = msg;
+}
+
+function bold(word) {
+  return '<strong>'+word+'</strong>';
 }
